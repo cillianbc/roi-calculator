fbe860eed1a6d0aa3f76e6bf398e68b7be542b1e
Cillian Bracken-Conway <cillianbc@gmail.com>
2017-08-28 15:57:05 +1000

    Version 0.1

--- js/app.js
+++ js/app.js
@@ -17,8 +17,30 @@ let incrementalSuccessAnnually = document.querySelector(".incrementalSuccessValu
 let roiannually = document.querySelector(".roiMonthly")
 let roiAnnually = document.querySelector(".roiAnnually")
 
-
-
+let state = {
+  currentValueMonthly:0,
+  currentValueAnnually:0,
+  estimatedFutureMonthly:0,
+  estimatedFutureAnnually:0,
+  incrementalValueMonthly:0,
+  incrementalValueAnnually:0,
+  incrementalSuccessMonthly:0,
+  incrementalSuccessAnnually:0,
+  roiMonthly:0,
+  roiAnnually:0
+}
+function stateRest(state) {
+    state.currentValueMonthly = 0;
+    state.currentValueAnnually = 0;
+    state.estimatedFutureMonthly = 0;
+    state.estimatedFutureAnnually = 0;
+    state.incrementalValueMonthly = 0;
+    state.incrementalValueAnnually = 0;
+    state.incrementalSuccessMonthly = 0;
+    state.incrementalSuccessAnnually = 0;
+    state.roiMonthly=0;
+    state.roiAnnually=0;
+}
 
 // finalUrl.onkeyup = function(){
 //   buildchart()
@@ -30,35 +52,88 @@ function numberWithCommas(x) {
 function loopClassesCurrentValue(month,annual){
   var month = document.querySelectorAll(month);
   var annual = document.querySelectorAll(annual);
+  state.currentValueMonthly = Number(enterAverageValueofaSuccessEventVisit.value) * Number(averageMonthlySuccessEvents.value)
+  state.currentValueAnnually = state.currentValueMonthly*12
   var index = 0, length = month.length;
     for ( ; index < length; index++) {
-      month[index].innerHTML = "$"+numberWithCommas(totalAverageMonthlySiteVisits.value * averageMonthlySuccessEvents.value)
+      month[index].innerHTML = "$"+numberWithCommas(state.currentValueMonthly)
     }
   var index = 0, length = annual.length;
     for ( ; index < length; index++) {
-      annual[index].innerHTML = "$"+(numberWithCommas((totalAverageMonthlySiteVisits.value * averageMonthlySuccessEvents.value)*12))
+      annual[index].innerHTML = "$"+numberWithCommas(state.currentValueAnnually)
     }
+  // var index = 0, length = annual.length;
+  //   for ( ; index < length; index++) {
+  //     annual[index].innerHTML = "$"+(numberWithCommas(state.currentValueAnnually)
+  //   }
 }
 
-function loopClassesFutureValue(month,annual){
+function loopClassesIncremental(incmonth,incannual,successmonth,successannual,roiAnnual,roiMonthly,state){
+  var incmonths = document.querySelectorAll(incmonth);
+  var incannuals = document.querySelectorAll(incannual);
+  var successmonths = document.querySelectorAll(successmonth);
+  var successannuals = document.querySelectorAll(successannual);
+  var roimonths = document.querySelectorAll(roiMonthly);
+  var roiannuals = document.querySelectorAll(roiAnnual);
+  console.log(successannuals)
+  var index = 0, length = incmonths.length;
+    for ( ; index < length; index++) {
+      incmonths[index].innerHTML = "$"+numberWithCommas(state.incrementalValueMonthly)
+    }
+  var index = 0, length = successmonths.length;
+    for ( ; index < length; index++) {
+      successmonths[index].innerHTML = numberWithCommas(state.incrementalSuccessMonthly)
+    }
+    var index = 0, length = incannuals.length;
+      for ( ; index < length; index++) {
+        incannuals[index].innerHTML = "$"+numberWithCommas(state.incrementalValueAnnually)
+      }
+    var index = 0, length = successannuals.length;
+      for ( ; index < length; index++) {
+        successannuals[index].innerHTML = numberWithCommas(state.incrementalSuccessAnnually)
+      }
+    var index = 0, length = roimonths.length;
+      for ( ; index < length; index++) {
+        roimonths[index].innerHTML = numberWithCommas(state.roiMonthly) +"%"
+      }
+    var index = 0, length = roiannuals.length;
+      for ( ; index < length; index++) {
+        roiannuals[index].innerHTML = numberWithCommas(state.roiAnnually) +"%"
+      }
+}
+
+function loopClassesFutureValue(month,annual,conversionRate){
   var month = document.querySelectorAll(month);
   var annual = document.querySelectorAll(annual);
-  var newperc = totalAverageMonthlySiteVisits.value * (1.+(increaseSiteTrafficbyX.value/100))
+  var newTrafficMonthly = totalAverageMonthlySiteVisits.value * (1.+(increaseSiteTrafficbyX.value/100))
+  var newConversionperc = Number(increaseConversionbyX.value) + conversionRate
+  var newConversionEvents = (newTrafficMonthly /100)*newConversionperc
+  var newMonthly = newConversionEvents*Number(enterAverageValueofaSuccessEventVisit.value)
+  var newAnnual = newMonthly*12
+  state.estimatedFutureMonthly = newMonthly
+  state.estimatedFutureAnnually = newAnnual
+  state.incrementalValueMonthly = state.estimatedFutureMonthly - state.currentValueMonthly
+  state.incrementalValueAnnually= state.estimatedFutureAnnually - state.currentValueAnnually
+  state.incrementalSuccessMonthly= newConversionEvents - Number(averageMonthlySuccessEvents.value)
+  state.incrementalSuccessAnnually= state.incrementalSuccessMonthly *12
+  state.roiMonthly = (state.estimatedFutureMonthly / estimatedCosttoImprovePerformance.value) *100
+  state.roiAnnually = state.roiMonthly *12
   var index = 0, length = month.length;
     for ( ; index < length; index++) {
-      month[index].innerHTML = "$"+numberWithCommas()
+      month[index].innerHTML = "$"+numberWithCommas(newMonthly)
     }
   var index = 0, length = annual.length;
     for ( ; index < length; index++) {
-      annual[index].innerHTML = "$"+(numberWithCommas((totalAverage.value * averageEvents.value)*12))
+      annual[index].innerHTML = "$"+numberWithCommas(newAnnual)
     }
+  loopClassesIncremental(".incrementalValueMonthly",".incrementalValueAnnually",".incrementalSuccessValueMonthly",".incrementalSuccessValueAnnually",".roiAnnually",".roiMonthly",state)
 }
 
 
 
 function populateData() {
-  successEventConversionRates.innerHTML = (totalAverageMonthlySiteVisits.value / averageMonthlySuccessEvents.value)/100
-  loopClassesCurrentValue(".currentValueMonthly",".currentValueAnnually",totalAverageMonthlySiteVisits,averageMonthlySuccessEvents)
-  // loopClassesFutureValue(".estimatedFutureMonthly",".estimatedFutureAnnually",)
-
+  conversionRate = (averageMonthlySuccessEvents.value / totalAverageMonthlySiteVisits.value)*100
+  successEventConversionRates.innerHTML = conversionRate
+  loopClassesCurrentValue(".currentValueMonthly",".currentValueAnnually",)
+  loopClassesFutureValue(".estimatedFutureMonthly",".estimatedFutureAnnually",conversionRate)
 }
